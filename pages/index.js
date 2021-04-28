d861395749aba01fdacc9889db86a689a2ab1d00
Abiseno Arya P <abiseno.arya@gmail.com>
2021-04-28 21:07:50 +0700

    Add Metas

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,21 @@ import { MainBanner } from "../components/MainBanner";
 import useMousePosition from "../hooks/useMousePosition";
 import Quote from "../components/Quote";
 import Faq from "../components/Faq";
+import Head from "next/head";
 
 export default function Home() {
   const { x, y } = useMousePosition();
 
   return (
     <>
+      <Head>
+        <title>Donasi 52</title>
+        <meta
+          name="description"
+          content="Donasi yang diselenggarakan oleh Smada 52 Yogyakarta"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <MainBanner x={x} y={y} />
       <Quote />
       <Faq />
